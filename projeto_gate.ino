ddf4bb6f10801ec74a9d17113c694e028c6c72a7
Anderson Duarte <alvesduarte97@gmail.com>
2019-03-10 21:03:06 -0300

    first commit

---
+++ projeto_gate.ino
@@ -0,0 +1,125 @@
+//Reles para ativar os motores
+int pnReleGateOpen = 3;
+int pnReleGateClose = 4;
+//Sensor de presenca
+int pnInfra = 5;
+//Fim de curso
+int pnEndLineOpen = 7;
+int pnEndLineClose = 6;
+//Botoes
+int pnButtonOpen = 8;
+int pnButtonClose = 9;
+//Rele do controle
+int pnReleControl = 2;
+//Variavel que define que o portao vai abrir
+int openGate = 1;
+
+int lastStatControl = 1;
+int statControl = 1;
+void setup()
+{
+    pinMode(pnReleControl, OUTPUT);
+    //digitalWrite(pnReleControl, HIGH);
+    
+    pinMode(pnInfra, OUTPUT);
+    
+    pinMode(pnEndLineOpen, OUTPUT);
+    pinMode(pnEndLineClose, OUTPUT);
+
+
+    pinMode(pnButtonOpen, INPUT);
+    digitalWrite(pnButtonOpen, HIGH);
+    
+    pinMode(pnButtonClose, OUTPUT);
+
+    pinMode(pnReleGateOpen, OUTPUT);
+    digitalWrite(pnReleGateOpen, HIGH);
+    pinMode(pnReleGateClose, OUTPUT);
+    digitalWrite(pnReleGateClose, HIGH);
+    
+
+
+    Serial.begin(9600);
+
+    close();
+}
+
+void loop()
+{
+  
+    statControl = digitalRead(pnReleControl);
+    
+//Caso o controle ative permanente o rele
+/*
+    if (statControl != lastStatControl)
+    {
+        lastStatControl = statControl;
+        if(digitalRead(pnEndLineOpen) == 1){
+            close();
+        }else if(digitalRead(pnEndLineClose) == 1){
+            open();
+        }
+
+    }
+*/
+
+//Caso o controle ative como se fosse click  
+/*
+    if (digitalRead(pnReleControl) == 0)
+    {
+        Serial.println("Inicial Open");
+        Serial.println(digitalRead(pnEndLineOpen));
+        Serial.println("Inicial Close");
+        Serial.println(digitalRead(pnEndLineClose));
+        lastStatControl = statControl;
+        
+        if(digitalRead(pnEndLineOpen) == 1){
+            close();
+        }else if(digitalRead(pnEndLineClose) == 1){
+            open();
+        }
+
+    } 
+
+    */
+    if(digitalRead(pnButtonOpen) == 0){
+        open();
+    }else if(digitalRead(pnButtonClose) == 0){
+        close();
+    }
+}
+
+
+void open(){
+    while(digitalRead(pnEndLineOpen) == 0){
+        Serial.println("Open");
+        Serial.println("EndLineOpen");
+        Serial.println(digitalRead(pnEndLineOpen));
+        //Ativando rele
+        digitalWrite(pnReleGateOpen, LOW);
+        delay(10);
+    }
+    //Desativando rele
+    digitalWrite(pnReleGateOpen, HIGH);
+}
+
+void close(){
+    
+    while(digitalRead(pnEndLineClose) == 0){
+        Serial.println("Close");
+        Serial.println("EndLineClose");
+        Serial.println(digitalRead(pnEndLineClose));
+        //Ativando rele
+        digitalWrite(pnReleGateClose, LOW);
+        delay(10);
+        //Condicao para se alguem passar na frente do portao quando ele estiver fechando
+        //Caso alguem passe, o portao ira abrir
+        if(digitalRead(pnInfra) == 1){
+            digitalWrite(pnReleGateClose, HIGH);
+            open();
+            return;
+        }
+    }
+    //Desativando rele
+    digitalWrite(pnReleGateClose, HIGH);
+}
